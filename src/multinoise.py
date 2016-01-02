293484425d8ba256a9aa4ae003be57c3c08f2b26
lara90 <laramk90@gmail.com>
2016-01-02 15:55:59 +0000

    Commenting
    Added comments

--- src/multinoise.py
+++ src/multinoise.py
@@ -14,11 +14,11 @@ for record in align:
 		out += str(record.seq[i])
 	sys.stdout.write(out+'\n')
 	
-if len(k) > 0:	
+if len(k) > 0:	#show how many columns are removed from the alignment
 	sys.stderr.write(str(l)+' columns were removed from this alignment\n')
-elif len(m) == 0:
+elif len(m) == 0: #error message when there is no record in the given alignment file
 	sys.stderr.write('Error: Empty file\n')
-elif len(k) == 0:
+elif len(k) == 0: #return a message if all columns are removed
 	sys.stderr.write('All columns were removed from this alignment\n')
 
 
